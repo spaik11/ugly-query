c55ce100c1e42be06bb1bc45675d3acffba1b4eb
Sung Paik <sung.paik@codeimmersives.com>
2019-10-08 15:56:05 -0400

    changing the background and titles

---
+++ main.js
@@ -0,0 +1,18 @@
+const body = document.querySelector('body')
+body.style.background = 'url(https://media.giphy.com/media/RqW2kzGBFL2es/giphy.gif)';
+
+const title = document.querySelector('h1');
+title.style.color = 'orange';
+title.style.letterSpacing = '100px';
+title.style.backgroundColor = 'teal';
+title.style.opacity = '.5';
+
+const div1 = document.querySelector('#div1');
+div1.style.color = 'brown';
+div1.style.fontSize = '100px';
+
+const para = document.querySelector('p');
+para.style.color = 'pink';
+para.style.backgroundColor = 'black';
+para.style.fontSize = '50px';
+para.style.padding = '250px';
