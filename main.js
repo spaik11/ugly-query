deb4b9af252681ff848d04a40513d9dad117dd91
Sung Paik <sung.paik@codeimmersives.com>
2019-10-08 20:55:17 -0400

    put class and id items

--- main.js
+++ main.js
@@ -7,12 +7,26 @@ title.style.letterSpacing = '100px';
 title.style.backgroundColor = 'teal';
 title.style.opacity = '.5';
 
+const para = document.querySelector('#p1');
+para.style.color = 'black';
+para.style.background = 'url(https://media.giphy.com/media/82MkOzEyyXeSLkgWyv/giphy.gif)';
+para.style.fontSize = '50px';
+para.style.padding = '400px';
+
 const div1 = document.querySelector('#div1');
 div1.style.color = 'brown';
 div1.style.fontSize = '100px';
 
-const para = document.querySelector('p');
-para.style.color = 'pink';
-para.style.backgroundColor = 'black';
-para.style.fontSize = '50px';
-para.style.padding = '250px';
+const div2 = document.querySelector('#div2');
+div2.style.color = 'purple';
+
+const div3 = document.querySelector('#div3');
+div3.style.color = 'red';
+div3.style.fontSize = '75px';
+
+const div4 = document.querySelector('#div4');
+div4.style.color = 'orange';
+div4.style.fontSize = '120px';
+
+const listedItems = document.querySelectorAll('li')
+listedItems.style.fontSize = '35px';
